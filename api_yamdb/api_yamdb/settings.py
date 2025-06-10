a6f113a39f1752494855bf490baf1d6e36658f89
Max Let <makcmans5@mail.ru>
2025-06-10 17:57:50 +0300

    Запятая

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
     'rest_framework',
     'reviews',
     'api',
